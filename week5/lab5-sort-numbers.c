b82c930423758425ca8952ae0a5b850e7e53c61d
ILIE <ilieg@gmail.com>
2025-10-12 12:01:43 +0000

    modified:   week5/lab5-sort-numbers.c

--- week5/lab5-sort-numbers.c
+++ week5/lab5-sort-numbers.c
@@ -5,26 +5,89 @@ Author: Ilie Gabuja
 #include <stdlib.h>
 #include <string.h>
 
-void asc(int *li[], int length);
-void desc(int *li[], int length);
-
+void desc(int li[], int length) // selection sort (O(n^2))
+{
+    int i = 0;
+    while (i < length)
+    {
+        int p = i;
+        int j = p + 1;
+        while (j < length)
+        {
+            if (*(li + j) > *(li + p))
+            {
+                p = j;
+            }
+            j++;
+        }
+        int temp = *(li + p);
+        *(li + p) = *(li + i);
+        *(li + i) = temp;
+        i++;
+    }
+    printf("Sorted numbers: ");
+    for (int i = 0; i < length; i++)
+    {
+        if (i != length - 1)
+        {
+            printf("%d ", *(li + i));
+        }
+        else
+        {
+            printf("%d", *(li + i));
+        }
+    }
+    printf("\n");
+}
+void asc(int li[], int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        int p = i;
+        int j = p + 1;
+        while (j < length)
+        {
+            if (*(li + j) < *(li + p))
+            {
+                p = j;
+            }
+            j++;
+        }
+        int temp = *(li + p);
+        *(li + p) = *(li + i);
+        *(li + i) = temp;
+        i++;
+    }
+    printf("Sorted numbers: ");
+    for (int i = 0; i < length; i++)
+    {
+        if (i != length - 1)
+        {
+            printf("%d ", *(li + i));
+        }
+        else
+        {
+            printf("%d", *(li + i));
+        }
+    }
+    printf("\n");
+}
 int main(int argc, char *argv[])
 {
     int length = atoi(argv[1]);
-    int li[length]; // declare a pointer to an array of length numbers
-    int sort[length];
-    char *which = argv[7];
+    int li[length];
     for (int i = 0; i < length; i++)
     {
         li[i] = atoi(argv[i + 2]);
     }
-    void (*pf)(int[], int) = {asc, desc};
-    if (strcmp(which, "asc") == 0)
+    if (strcmp(argv[argc - 1], "asc") == 0)
     {
-        pf = asc;
+        asc(li, length);
     }
-    else
+    else if (strcmp(argv[argc - 1], "desc") == 0)
     {
-        pf = desc;
+        desc(li, length);
     }
+    return 0;
 }
